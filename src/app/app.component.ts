dc405ad7bfca4c319b5085a46eb365820850ed11
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-10 15:18:44 +0300

    motion detect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { SweetalertService, SweetIconType, SweetPosition } from './services/common/sweetalert.service';
+import { PluginListenerHandle } from '@capacitor/core';
+import { Motion } from '@capacitor/motion';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  isMotion: string = "";
+  constructor() {
+    this.motion();
+  }
+  async motion() {
+    await Motion.addListener("accel", event => {
+      this.isMotion = "Device motion event: " + event;
+    })
+  }
 }
