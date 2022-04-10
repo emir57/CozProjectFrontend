e50e83ab9e4449ff4f79b8942f04118800c82c12
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-10 14:11:35 +0300

    showMessage function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,9 @@
 import { Component } from '@angular/core';
-import * as sweetalert from "../../node_modules/sweetalert2/dist/sweetalert2.min.js"
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
-    this.showMessage();
-  }
-  showMessage() {
-    const Toast = sweetalert.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.addEventListener('mouseenter', sweetalert.stopTimer)
-        toast.addEventListener('mouseleave', sweetalert.resumeTimer)
-      }
-    })
-
-    Toast.fire({
-      icon: 'success',
-      title: 'Signed in successfully'
-    })
-  }
+  constructor() {}
 }
--- src/app/services/sweetalert.service.ts
+++ src/app/services/sweetalert.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import * as sweetalert from "../../../node_modules/sweetalert2/dist/sweetalert2.min.js"
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,22 @@ import { Injectable } from '@angular/core';
 export class SweetalertService {
 
   constructor() { }
+  showMessage() {
+    const Toast = sweetalert.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', sweetalert.stopTimer)
+        toast.addEventListener('mouseleave', sweetalert.resumeTimer)
+      }
+    })
+
+    Toast.fire({
+      icon: 'success',
+      title: 'Signed in successfully'
+    })
+  }
 }
