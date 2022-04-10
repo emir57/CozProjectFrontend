aabfefb2f213bef4b7becfa8427b16ebed27579d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-10 14:07:13 +0300

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as sweetalert from "../../node_modules/sweetalert2/dist/sweetalert2.min.js"
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   constructor() {
+    this.showMessage();
+  }
+  showMessage() {
+    const Toast = sweetalert.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', sweetalert.stopTimer)
+        toast.addEventListener('mouseleave', sweetalert.resumeTimer)
+      }
+    })
 
+    Toast.fire({
+      icon: 'success',
+      title: 'Signed in successfully'
+    })
   }
 }
