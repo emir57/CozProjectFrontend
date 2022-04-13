106f1e94f38661c99d9410718c13374df3ed6093
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-13 15:47:23 +0300

    Storage SetName

--- src/app/services/common/storage.service.ts
+++ src/app/services/common/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@capacitor/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,11 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  async setName(key:string,value:any) {
+    await Storage.set({
+      key:key,
+      value:JSON.stringify(value)
+    });
+  }
 }
