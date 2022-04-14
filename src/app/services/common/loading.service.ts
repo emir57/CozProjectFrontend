dbddc070c03c8ee6addf006464dc0baefd4cc5ee
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 16:06:32 +0300

    loadingControl

--- src/app/services/common/loading.service.ts
+++ src/app/services/common/loading.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
 
-  constructor() { }
+  constructor(
+    private loadingControl: LoadingController
+  ) { }
+
+  async showLoading(message: string) {
+    const loading = await this.loadingControl.create({
+      message: message
+    })
+    await loading.present;
+  }
+
+  async closeLoading() {
+    this.loadingControl.dismiss();
+  }
 }
