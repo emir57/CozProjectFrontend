eba0581c89443aaaf63da851458e25def00323d3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-14 10:30:41 +0300

    readAsBase64

--- src/app/services/common/image-upload.service.ts
+++ src/app/services/common/image-upload.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Platform } from '@ionic/angular';
+import { fileURLToPath } from 'url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageUploadService {
 
-  constructor() { }
+  constructor(private platform: Platform) { }
 
   async selectImage() {
     const image = await Camera.getPhoto({
@@ -23,12 +25,23 @@ export class ImageUploadService {
   }
 
   async saveImage(photo: Photo) {
+    const base46Data = "";
     const fileName = new Date().getTime() + ".jpeg";
     const savedFile = await Filesystem.writeFile({
       directory: Directory.Data,
       path: `${IMAGE_DIR}/${fileName}`,
-      data: base64
+      data: base46Data
     })
+  }
+
+  async readAsBase64(photo: Photo) {
+    if (this.platform.is("hybrid")) {
+      const file = await Filesystem.readFile({
+        path: photo.path
+      })
+      return file.data;
+    };
+
   }
 
 
