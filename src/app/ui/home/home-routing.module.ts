07e24f70fc1e1d047039929ce72ef3d3c312e78a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-15 17:38:10 +0300

    Home component can Activate guard-> IsLoginGuard

--- src/app/ui/home/home-routing.module.ts
+++ src/app/ui/home/home-routing.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IsLoginGuard } from 'src/app/guards/is-login.guard';
 
 import { HomePage } from './home.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomePage
+    component: HomePage,
+    canActivate: [IsLoginGuard]
   },
   {
     path: 'questions',
-    loadChildren: () => import('./questions/questions.module').then( m => m.QuestionsPageModule)
+    loadChildren: () => import('./questions/questions.module').then(m => m.QuestionsPageModule)
   },
   {
     path: 'profile',
-    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfilePageModule)
   },
   {
     path: 'stats',
-    loadChildren: () => import('./stats/stats.module').then( m => m.StatsPageModule)
+    loadChildren: () => import('./stats/stats.module').then(m => m.StatsPageModule)
   },
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule)
   }
 ];
 
@@ -30,4 +32,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class HomePageRoutingModule {}
+export class HomePageRoutingModule { }
