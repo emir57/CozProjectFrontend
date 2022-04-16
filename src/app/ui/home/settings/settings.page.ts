77ac41d0aeb4dd4b94535c2648872d9fa4a86509
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 16:43:58 +0300

    logout

--- src/app/ui/home/settings/settings.page.ts
+++ src/app/ui/home/settings/settings.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AlertService } from 'src/app/services/common/alert-service.service';
+import { AuthService } from 'src/app/services/common/auth.service';
 import { RoleService } from 'src/app/services/common/role.service';
+import { KeyType, StorageService } from 'src/app/services/common/storage.service';
+import { SweetalertService, SweetIconType } from 'src/app/services/common/sweetalert.service';
 
 @Component({
   selector: 'app-settings',
@@ -13,23 +17,33 @@ export class SettingsPage implements OnInit {
   roles: string[] = [];
   constructor(
     private roleService: RoleService,
-    private alertService:AlertService
+    private alertService: AlertService,
+    private authService: AuthService,
+    private storageService: StorageService,
+    private router: Router,
+    private messageService: SweetalertService
   ) { }
 
   ngOnInit() {
 
   }
 
-  isAdmin(){
+  isAdmin() {
     return this.roleService.isAdmin;
   }
-  isTeacher(){
+  isTeacher() {
     return this.roleService.isTeacher;
   }
 
-  logout(){
+  async logout() {
     this.alertService.showAlertConfirm("Çıkış yapmak istediğinizden eminmisiniz",
-    (cancel)=>{console.log("cancel")},
-    (ok)=>{console.log("ok")})
+      (cancel) => { console.log("cancel") },
+      async (ok) => {
+        this.authService.setIsLogin(false);
+        await this.storageService.removeName(KeyType.Token);
+        await this.storageService.removeName(KeyType.User);
+        this.router.navigateByUrl("/login")
+        this.messageService.showMessage("Çıkış Başarılı", { iconType: SweetIconType.Success })
+      })
   }
 }
