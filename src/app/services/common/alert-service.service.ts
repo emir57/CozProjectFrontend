553bbe8985e2f325aa05e4a8365b3977ebf8001e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 16:39:17 +0300

    showAlertConfirm

--- src/app/services/common/alert-service.service.ts
+++ src/app/services/common/alert-service.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertSErviceService {
 
-  constructor() { }
+  constructor(
+    private alertController: AlertController
+  ) { }
+  async showAlertConfirm(message: string, cancelFunction: Function, okFunction: Function) {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'İptal',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: cancelFunction()
+        }, {
+          text: 'Okay',
+          handler: okFunction()
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
