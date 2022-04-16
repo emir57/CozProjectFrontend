1560455597aef3e54a4e7944611865f620c97016
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 22:08:12 +0300

    getDate function

--- src/app/admin/category-save/category-save.page.ts
+++ src/app/admin/category-save/category-save.page.ts
@@ -40,4 +40,9 @@ export class CategorySavePage implements OnInit {
     }
   }
 
+  getDate(dateString:string){
+    let date = new Date(dateString);
+    return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours}:${date.getMinutes()}`;
+  }
+
 }
