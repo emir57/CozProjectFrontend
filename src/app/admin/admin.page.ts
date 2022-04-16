557d2173568d8b9ccf7ff183d110e5096fbb0233
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 17:02:01 +0300

    Admin ExitPanel

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertService } from '../services/common/alert-service.service';
 
 @Component({
   selector: 'app-admin',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit() {
+  }
+
+  async exitPanel() {
+    await this.alertService.showAlertConfirm("Panelden çıkmak üzeresiniz","Panelden Çıkılıyor",
+    ()=>{},
+    ()=>{
+      this.router.navigateByUrl("/home/questions")
+    })
   }
 
 }
