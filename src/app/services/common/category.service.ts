71337a328ceebf35d8918aa848aadb609de70947
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 22:15:43 +0300

    getall

--- src/app/services/common/category.service.ts
+++ src/app/services/common/category.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import ResponseListModel from 'src/app/models/responseListModel';
 import ResponseModel from 'src/app/models/responseModel';
 import ResponseSingleModel from 'src/app/models/responseSingleModel';
 import { CategoryModel } from 'src/app/models/tables/categoryMode';
@@ -29,5 +30,9 @@ export class CategoryService {
   getById(categoryId: number) {
     let url = `${this.baseUrl}api/categories/getbyid?id=${categoryId}`;
     return this.http.get<ResponseSingleModel<CategoryModel>>(url);
+  }
+  getall() {
+    let url = `${this.baseUrl}api/categories/getall`;
+    return this.http.get<ResponseListModel<CategoryModel>>(url);
   }
 }
