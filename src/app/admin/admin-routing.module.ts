c76de5783badb52b4def55d7576a2bfa3753ddac
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 16:48:39 +0300

    admin answers and questions

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
   {
     path: 'categories',
     loadChildren: () => import('./categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'answers',
+    loadChildren: () => import('./answers/answers.module').then( m => m.AnswersPageModule)
+  },
+  {
+    path: 'questions',
+    loadChildren: () => import('./questions/questions.module').then( m => m.QuestionsPageModule)
   }
 ];
 
