5a556091ef2ddfb7795fd7881d0b6a7377d90bee
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 00:45:55 +0300

    app component check user and token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,21 @@ import { KeyType, StorageService } from './services/common/storage.service';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor() {
+  constructor(
+    private storageService: StorageService,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
+    this.checkStorage();
+  }
 
+  async checkStorage() {
+    const user = await this.storageService.checkName(KeyType.User);
+    const token = await this.storageService.checkName(KeyType.Token);
+    if (user && token) {
+      this.router.navigateByUrl("/home/questions")
+    }
   }
 }
