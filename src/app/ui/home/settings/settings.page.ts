003603529f8c51f02683ad93257c1fe6bef4d8ea
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 16:56:06 +0300

    change admin url

--- src/app/ui/home/settings/settings.page.ts
+++ src/app/ui/home/settings/settings.page.ts
@@ -35,7 +35,7 @@ export class SettingsPage implements OnInit {
     return this.roleService.isTeacher;
   }
   goAdminPage() {
-    this.router.navigateByUrl("/admin");
+    this.router.navigateByUrl("/admin/categories");
   }
 
   async logout() {
