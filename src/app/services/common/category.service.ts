938b116b8c7a9242d6c8031fe84a75f3bb1691de
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-16 22:12:35 +0300

    category add service

--- src/app/services/common/category.service.ts
+++ src/app/services/common/category.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import ResponseModel from 'src/app/models/responseModel';
+import { CategoryModel } from 'src/app/models/tables/categoryMode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
+
+  add(categoryModel: CategoryModel) {
+    let url = `${this.baseUrl}api/categories/add`;
+    return this.http.post<ResponseModel>(url, categoryModel)
+  }
 }
