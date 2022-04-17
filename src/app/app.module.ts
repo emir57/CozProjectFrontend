40279963ba8e76aed498121cf1f5e2f61c6cc8eb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 13:39:00 +0300

    add interceptor provide

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { CustomInterceptorDirective } from './interceptors/custom-interceptor.directive'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptorService } from './interceptors/http-interceptor.service';
 
 @NgModule({
-  declarations: [AppComponent, CustomInterceptorDirective],
+  declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
@@ -20,7 +20,8 @@ import { CustomInterceptorDirective } from './interceptors/custom-interceptor.di
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: "baseUrl", useValue: "https://localhost:44379/" }
+    { provide: "baseUrl", useValue: "https://localhost:44379/" },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }
   ],
   bootstrap: [AppComponent],
 })
