b898e4f666273c94b867d497bcd1b818f467ff5a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 14:32:13 +0300

    CheckTeacherRoleGuard

--- src/app/guards/check-teacher-role-guard.ts
+++ src/app/guards/check-teacher-role-guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { RoleService } from '../services/common/role.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CheckTeacherRoleGuardGuard implements CanActivate {
+export class CheckTeacherRoleGuard implements CanActivate {
+  constructor(
+    private roleService: RoleService
+  ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    let isTeacher = this.roleService.isTeacher;
+    if (isTeacher) {
+      return true;
+    }
+    return false;
   }
-  
+
 }
