c63fc7d81028ff654db35fcd051e5ba065d1621d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 14:31:11 +0300

    CheckAdminRoleGuard

--- src/app/guards/check-admin-role.guard.ts
+++ src/app/guards/check-admin-role.guard.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { RoleService } from '../services/common/role.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckAdminRoleGuard implements CanActivate {
+  constructor(
+    private roleService: RoleService
+  ) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    let isAdmin = this.roleService.isAdmin;
+    if (isAdmin) {
+      return true;
+    }
+    return false;
   }
-  
+
 }
