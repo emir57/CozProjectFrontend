ba20df5a58b9bfd8cbb83c3f1066058d825ddbe9
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 13:39:33 +0300

    interceptor import storageService

--- src/app/interceptors/http-interceptor.service.ts
+++ src/app/interceptors/http-interceptor.service.ts
@@ -1,13 +1,18 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import TokenModel from '../models/auth/tokenModel';
+import { StorageService } from '../services/common/storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  token: TokenModel
+  constructor(
+    private storageService: StorageService
+  ) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     throw new Error('Method not implemented.');
   }
