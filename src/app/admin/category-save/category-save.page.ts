327af1c6a828ff25f3c86010448780617dc18d26
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-17 13:32:13 +0300

    category save unauthorized exception

--- src/app/admin/category-save/category-save.page.ts
+++ src/app/admin/category-save/category-save.page.ts
@@ -69,7 +69,7 @@ export class CategorySavePage implements OnInit {
           this.messageService.showMessage(error.ErrorMessage, { iconType: SweetIconType.Error })
         }
       } else {
-        this.messageService.showMessage(responseErr.error.message, { iconType: SweetIconType.Error })
+        this.messageService.showMessage(responseErr.error.Message, { iconType: SweetIconType.Error })
       }
       await this.loadingService.closeLoading();
       this.isOk = true;
@@ -89,7 +89,7 @@ export class CategorySavePage implements OnInit {
           this.messageService.showMessage(error.ErrorMessage, { iconType: SweetIconType.Error })
         }
       } else {
-        this.messageService.showMessage(responseErr.error.message, { iconType: SweetIconType.Error })
+        this.messageService.showMessage(responseErr.error.Message, { iconType: SweetIconType.Error })
       }
       await this.loadingService.closeLoading();
       this.isOk = true;
