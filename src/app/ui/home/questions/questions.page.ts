b3aebdd10d5ccd50f1dead19823399d57cc29956
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:50:16 +0300

    get categories

--- src/app/ui/home/questions/questions.page.ts
+++ src/app/ui/home/questions/questions.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { CategoryModel } from 'src/app/models/tables/categoryMode';
+import { CategoryService } from 'src/app/services/common/category.service';
 
 @Component({
   selector: 'app-questions',
@@ -8,11 +10,21 @@ import { MenuController } from '@ionic/angular';
 })
 export class QuestionsPage implements OnInit {
 
+  categories: CategoryModel[] = []
   constructor(
-
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getall().subscribe(response => {
+      if (response.success) {
+        this.categories = response.data;
+      }
+    })
   }
 
 }
