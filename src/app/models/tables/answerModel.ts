d7568307c0255bda7ed482bb7bf52581fe985803
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:06:11 +0300

    Create AnswerModel

---
+++ src/app/models/tables/answerModel.ts
@@ -0,0 +1,9 @@
+export interface AnswerModel {
+  id: number;
+  questionId: number;
+  content: string;
+  isTrue: boolean;
+  createdDate: string;
+  updatedDate: string;
+  deletedDate: string;
+}
