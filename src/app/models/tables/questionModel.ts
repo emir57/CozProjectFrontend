eac25a600238cdecc0136a041ad502442b355c92
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:07:50 +0300

    QuestionModel add answers

--- src/app/models/tables/questionModel.ts
+++ src/app/models/tables/questionModel.ts
@@ -1,10 +1,13 @@
+import { AnswerModel } from "./answerModel";
+
 export interface QuestionModel {
-  id:number;
-  content:string;
-  teacherId:number;
-  categoryId:number;
-  score:number;
-  createdDate:string;
-  updatedDate:string;
-  deletedDate:string;
+  id: number;
+  content: string;
+  teacherId: number;
+  categoryId: number;
+  score: number;
+  createdDate: string;
+  updatedDate: string;
+  deletedDate: string;
+  answers: AnswerModel[]
 }
