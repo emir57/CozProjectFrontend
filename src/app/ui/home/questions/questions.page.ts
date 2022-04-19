e6227adadcaa56857d1b85d996c99423e6f1f268
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 16:51:54 +0300

    add animations

--- src/app/ui/home/questions/questions.page.ts
+++ src/app/ui/home/questions/questions.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { CategoryModel } from 'src/app/models/tables/categoryMode';
 import { CategoryService } from 'src/app/services/common/category.service';
+declare var $: any;
 
 @Component({
   selector: 'app-questions',
@@ -23,7 +24,30 @@ export class QuestionsPage implements OnInit {
     this.categoryService.getall().subscribe(response => {
       if (response.success) {
         this.categories = response.data;
+        setTimeout(() => {
+          this.animationArrows();
+        }, 0);
       }
+    })
+  }
+  animationArrows() {
+    let arrows1 = $(".arrow1");
+    let arrows2 = $(".arrow2");
+    setInterval(() => {
+      setTimeout(() => {
+        arrows1.animate({
+          opacity: 1
+        }, 500)
+        arrows2.animate({
+          opacity: 1
+        }, 800)
+        arrows1.animate({
+          opacity: 0
+        }, 800)
+        arrows2.animate({
+          opacity: 0
+        }, 500)
+      }, 1000);
     })
   }
 
