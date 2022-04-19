9553e03f31caa27048a5577f421f9d537f4ffcc2
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-19 18:04:25 +0300

    Create QuestionModel

---
+++ src/app/models/tables/questionModel.ts
@@ -0,0 +1,10 @@
+export interface QuestionModel {
+  id:number;
+  content:string;
+  teacherId:number;
+  categoryId:number;
+  score:number;
+  createdDate:string;
+  updatedDate:string;
+  deletedDate:string;
+}
