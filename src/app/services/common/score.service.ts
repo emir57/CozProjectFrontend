231e5bb529a7ba8a66d6d2315fb58ba68f194009
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-20 16:44:29 +0300

    Score Service

--- src/app/services/common/score.service.ts
+++ src/app/services/common/score.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import ResponseModel from 'src/app/models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScoreService {
 
-  constructor() { }
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
+
+  updateScore(updateScoreModel: UpdateScoreModel) {
+    let url = `${this.baseUrl}api/users/updatescore`;
+    return this.http.post<ResponseModel>(url, updateScoreModel);
+  }
+}
+
+export interface UpdateScoreModel {
+  userId: number;
+  questionId: number;
+  result: boolean;
+  score: number;
 }
