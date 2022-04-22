60b8423c236bc0979a55ba83e5c70baebbb7e679
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-22 15:49:58 +0300

    fix modules problem

--- src/app/admin/questions/questions.module.ts
+++ src/app/admin/questions/questions.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,10 +11,11 @@ import { QuestionsPage } from './questions.page';
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     IonicModule,
     QuestionsPageRoutingModule
   ],
   declarations: [QuestionsPage]
 })
-export class QuestionsPageModule {}
+export class QuestionsPageModule { }
