96e5998ba75d170f055130131853fbe2a7df5c4c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 21:17:44 +0300

    create user model

---
+++ src/app/models/tables/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  emailConfirmed: boolean;
+  score: number;
+  profilePhotoUrl: string;
+  createdDate: string;
+  updatedDate: string;
+  deletedDate: string;
+}
