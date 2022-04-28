f754a89d6e1f9b44c73027edcf25c2526afb842d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-28 21:31:04 +0300

    user service getAll

--- src/app/services/common/user.service.ts
+++ src/app/services/common/user.service.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@microsoft/signalr';
+import ResponseListModel from 'src/app/models/responseListModel';
+import ResponseModel from 'src/app/models/responseModel';
 import { User } from 'src/app/models/tables/user';
+import { SweetalertService, SweetIconType } from './sweetalert.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +12,20 @@ export class UserService {
 
   constructor(
     @Inject("baseUrl") private baseUrl: string,
-    private http: HttpClient
+    private http: HttpClient,
+    private messageService: SweetalertService
   ) { }
 
-  getAll() {
+  getAll(successCallback: (users: User[]) => void, errorCallBack: (err: any) => void) {
     let url = `${this.baseUrl}api/users/getall`;
-    
+    this.http.get<ResponseListModel<User>>(url).subscribe(response => {
+      if (response.success) {
+        successCallback(response.data);
+      }
+    }, (responseErr) => {
+      this.messageService.showMessage(responseErr.error.message, { iconType: SweetIconType.Error })
+      errorCallBack(responseErr);
+    })
   }
+
 }
