dec861a4b004fcd976d232daeba6a9dfb5681224
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-11 20:04:59 +0300

    declare and export QuestionModule

--- src/app/ui/home/questions/question/question.module.ts
+++ src/app/ui/home/questions/question/question.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuestionComponent } from './question.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    QuestionComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    QuestionComponent
   ]
 })
 export class QuestionModule { }
