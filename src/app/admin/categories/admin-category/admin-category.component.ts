c9d34c9e9f82ff32039de10ef9262d35bb2936da
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-11 20:27:18 +0300

    AdminCategoryComponent

--- src/app/admin/categories/admin-category/admin-category.component.ts
+++ src/app/admin/categories/admin-category/admin-category.component.ts
@@ -1,16 +1,58 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, Output } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { CategoryModel } from 'src/app/models/tables/categoryModel';
+import { EventEmitter } from 'stream';
+import { CategorySavePage } from '../../category-save/category-save.page';
+declare var $: any;
 
 @Component({
   selector: 'app-admin-category',
   templateUrl: './admin-category.component.html',
   styleUrls: ['./admin-category.component.scss'],
 })
-export class AdminCategoryComponent implements OnInit {
+export class AdminCategoryComponent implements OnInit,AfterViewInit {
 
   @Input() category: CategoryModel;
-  constructor() { }
+  @Output() getCategories: EventEmitter = new EventEmitter();
+  constructor(
+    private modalController: ModalController
+  ) { }
 
-  ngOnInit() { }
+  ngAfterViewInit(): void {
+    this.animationArrows();
+  }
 
+  ngOnInit() {}
+
+  async categoryEditPage(category: CategoryModel) {
+    const modal = await this.modalController.create({
+      component: CategorySavePage,
+      componentProps: { category: category }
+    })
+    modal.onDidDismiss().then(() => {
+
+    })
+    return await modal.present();
+  }
+
+  animationArrows() {
+    let arrows1 = $(".arrow1");
+    let arrows2 = $(".arrow2");
+    setInterval(() => {
+      setTimeout(() => {
+        arrows1.animate({
+          opacity: 1
+        }, 500)
+        arrows2.animate({
+          opacity: 1
+        }, 800)
+        arrows1.animate({
+          opacity: 0
+        }, 800)
+        arrows2.animate({
+          opacity: 0
+        }, 500)
+      }, 1000);
+    })
+  }
 }
--- src/app/admin/categories/categories.page.ts
+++ src/app/admin/categories/categories.page.ts
@@ -30,21 +30,10 @@ export class CategoriesPage implements OnInit {
       if (response.success) {
         this.categories = response.data;
         setTimeout(() => {
-          this.animationArrows();
+
         }, 0);
       }
     })
-  }
-
-  async categoryEditPage(category: CategoryModel) {
-    const modal = await this.modalController.create({
-      component: CategorySavePage,
-      componentProps: { category: category }
-    })
-    modal.onDidDismiss().then(()=>{
-      this.getCategories();
-    })
-    return await modal.present();
   }
   getBgColor(category: CategoryModel) {
     console.log(category.backgroundColor)
@@ -54,24 +43,4 @@ export class CategoriesPage implements OnInit {
     return "color:" + category.textColor + ";"
   }
 
-  animationArrows() {
-    let arrows1 = $(".arrow1");
-    let arrows2 = $(".arrow2");
-    setInterval(() => {
-      setTimeout(() => {
-        arrows1.animate({
-          opacity: 1
-        }, 500)
-        arrows2.animate({
-          opacity: 1
-        }, 800)
-        arrows1.animate({
-          opacity: 0
-        }, 800)
-        arrows2.animate({
-          opacity: 0
-        }, 500)
-      }, 1000);
-    })
-  }
 }
