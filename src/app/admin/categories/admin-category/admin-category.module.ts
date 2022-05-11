c5b8c29bace4c8c0de2ff8d10844d5dde8b48a4b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-11 20:21:14 +0300

    AdminCategoryModule

--- src/app/admin/categories/admin-category/admin-category.module.ts
+++ src/app/admin/categories/admin-category/admin-category.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminCategoryComponent } from './admin-category.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdminCategoryComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    AdminCategoryComponent
   ]
 })
 export class AdminCategoryModule { }
