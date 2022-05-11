9434036303497384b0c3b7078c0008a1f5c0c543
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-11 20:09:46 +0300

    Question component

--- src/app/ui/home/questions/question/question.component.ts
+++ src/app/ui/home/questions/question/question.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CategoryModel } from 'src/app/models/tables/categoryModel';
+import { User } from 'src/app/models/tables/user';
+import { QuestionPage } from 'src/app/question/question.page';
+import { KeyType, StorageService } from 'src/app/services/common/storage.service';
 
 @Component({
   selector: 'app-question',
@@ -7,8 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  @Input() category: CategoryModel
+  user: User;
+  constructor(
+    private modalController: ModalController,
+    private storageService: StorageService
+  ) { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    await this.getUser();
+  }
+
+  async getUser() {
+    this.user = JSON.parse(await this.storageService.checkName(KeyType.User));
+  }
+
+  checkCompleteDiv(category: CategoryModel) {
+    if (category.isComplete) {
+      return `
+      z-index: 99;
+      background-color: black;
+      opacity: .3; `;
+    }
+  }
+  checkCompleteSpan(category: CategoryModel) {
+    if (category.isComplete) {
+      return `
+      z-index: 102;
+      color:black;
+      float:right;
+      `;
+    }
+  }
+  async openAnswerModal(category: CategoryModel) {
+    const modal = await this.modalController.create({
+      component: QuestionPage,
+      componentProps: { category: category, user: this.user }
+    })
+    await modal.present();
+  }
 
 }
