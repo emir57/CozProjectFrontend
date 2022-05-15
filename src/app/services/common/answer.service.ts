5039e516114c226e377fe794f93b3f07eac3295f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-15 13:35:38 +0300

    fix HttpClient

--- src/app/services/common/answer.service.ts
+++ src/app/services/common/answer.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import ResponseListModel from 'src/app/models/responseListModel';
+import { AnswerModel } from 'src/app/models/tables/answerModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnswerService {
 
-  constructor() { }
+  constructor(
+    @Inject("baseUrl") private baseUrl: string,
+    private http: HttpClient
+  ) { }
+
+  getAll(
+    successCallBack?: (response: ResponseListModel<AnswerModel>) => void,
+    errorCallBack?: (responseErr: HttpErrorResponse) => void) {
+    let url = `${this.baseUrl}api/answers/getall`;
+    this.http.get<ResponseListModel<AnswerModel>>(url).subscribe();
+  }
 }
