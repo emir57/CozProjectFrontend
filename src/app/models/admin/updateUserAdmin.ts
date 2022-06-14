8353cab96f5cbc5de0e6589869eaffe793ae047c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-14 13:39:47 +0300

    Create UpdateUserAdmin Model

---
+++ src/app/models/admin/updateUserAdmin.ts
@@ -0,0 +1,9 @@
+export interface UpdateUserAdmin {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  emailConfirmed: boolean;
+  score: number;
+  profilePhotoUrl: string;
+}
