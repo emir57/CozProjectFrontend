18cd80365755a9e0445170fd29398446524f73b2
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-14 13:41:50 +0300

    user-save apge inject services

--- src/app/admin/user-save/user-save.page.ts
+++ src/app/admin/user-save/user-save.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { LoadingService } from 'src/app/services/common/loading.service';
+import { SweetalertService } from 'src/app/services/common/sweetalert.service';
+import { UserService } from 'src/app/services/common/user.service';
 
 @Component({
   selector: 'app-user-save',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSavePage implements OnInit {
 
-  constructor() { }
+  @Input() userId: number;
+  constructor(
+    private modalController: ModalController,
+    private messageService: SweetalertService,
+    private userService: UserService,
+    private loadingService: LoadingService
+  ) { }
 
   ngOnInit() {
+  }
+
+  update() {
+
   }
 
 }
