dde530dab9d64b51a9ffe7289516ea8b1c64fea7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-14 13:53:15 +0300

    users page editUserModal

--- src/app/admin/users/users.page.ts
+++ src/app/admin/users/users.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 import { User } from 'src/app/models/tables/user';
 import { UserService } from 'src/app/services/common/user.service';
+import { UserSavePage } from '../user-save/user-save.page';
 
 @Component({
   selector: 'app-users',
@@ -12,6 +14,7 @@ export class UsersPage implements OnInit {
   users: User[]
   constructor(
     private userService: UserService,
+    private modalController: ModalController
   ) { }
 
   ngOnInit() {
@@ -30,6 +33,20 @@ export class UsersPage implements OnInit {
   getDate(dateString: string) {
     let date = new Date(dateString);
     return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`
+  }
+
+  async editUserModal(userId: number) {
+    const modal = await this.modalController.create({
+      component: UserSavePage,
+      componentProps: { userId: 6 }
+    })
+    modal.onDidDismiss().then(async value=>{
+      if(value.data){
+        await this.getUsers();
+      }
+    })
+
+    return await modal.present();
   }
 
 }
