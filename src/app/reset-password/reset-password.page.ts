95f32d1094e72f1d228489f63c091d14f53eb6a8
emir57 <emir.gurbuz06@hotmail.com>
2022-08-04 13:52:39 +0300

    Complete ResetPasswordPage and profile page create modals

--- src/app/reset-password/reset-password.page.ts
+++ src/app/reset-password/reset-password.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { User } from '../models/tables/user';
+import { UserResetPasswordModel } from '../models/tables/userResetPasswordModel';
+import { KeyType, StorageService } from '../services/common/storage.service';
+import { SweetalertService, SweetIconType } from '../services/common/sweetalert.service';
+import { UserService } from '../services/common/user.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
 
-  constructor() { }
+  currentUser: User;
+  isOk: boolean = true;
+  resetPasswordForm: FormGroup;
+  constructor(
+    private messageService: SweetalertService,
+    private storageService: StorageService,
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.createResetPasswordForm();
+    this.currentUser = JSON.parse(await this.storageService.getValue(KeyType.User));
+  }
+
+  createResetPasswordForm() {
+    this.resetPasswordForm = this.formBuilder.group({
+      oldPassword: ["", [Validators.required, Validators.minLength(5)]],
+      newPassword: ["", [Validators.required, Validators.minLength(5)]],
+      newRePassword: ["", [Validators.required, Validators.minLength(5)]]
+    }, { validators: this.checkpassword })
+  }
+
+  resetPassword() {
+    if (this.resetPasswordForm.valid) {
+      this.isOk = false;
+      let resetPasswordModel: UserResetPasswordModel = this.resetPasswordForm.value;
+      resetPasswordModel.email = this.currentUser.email;
+      this.userService.resetPassword(resetPasswordModel,
+        (responseErr) => {
+          this.isOk = true;
+          this.messageService.showMessage(responseErr.error.message, { iconType: SweetIconType.Error });
+        }, (response) => {
+          this.isOk = true;
+          this.messageService.showMessage(response.message, { iconType: SweetIconType.Success });
+        })
+    }
+  }
+
+  checkpassword: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    let password = group.get("newPassword").value;
+    let repassword = group.get("newRePassword").value;
+    return password === repassword ? null : { notSame: true };
   }
 
 }
--- src/app/ui/home/profile/profile.page.ts
+++ src/app/ui/home/profile/profile.page.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
-import ResponseModel from 'src/app/models/responseModel';
-import { UpdateUserModel } from 'src/app/models/tables/updateUserModel';
+import { ModalController } from '@ionic/angular';
 import { User } from 'src/app/models/tables/user';
-import { UserResetPasswordModel } from 'src/app/models/tables/userResetPasswordModel';
-import { KeyType, StorageService } from 'src/app/services/common/storage.service';
-import { SweetalertService, SweetIconType } from 'src/app/services/common/sweetalert.service';
-import { UserService } from 'src/app/services/common/user.service';
+import { ResetPasswordPage } from 'src/app/reset-password/reset-password.page';
+import { UpdateProfilePage } from 'src/app/update-profile/update-profile.page';
 
 @Component({
   selector: 'app-profile',
@@ -16,54 +12,24 @@ import { UserService } from 'src/app/services/common/user.service';
 export class ProfilePage implements OnInit {
 
   currentUser: User;
-  isOk: boolean = true;
-  saveForm: FormGroup;
-  resetPasswordForm: FormGroup;
   constructor(
-    private formBuilder: FormBuilder,
-    private userService: UserService,
-    private storageService: StorageService,
-    private messageService: SweetalertService
+    private modalController: ModalController
   ) { }
 
   async ngOnInit() {
-    this.createResetPasswordForm();
-    this.currentUser = JSON.parse(await this.storageService.getValue(KeyType.User));
-  }
-
-  createResetPasswordForm() {
-    this.resetPasswordForm = this.formBuilder.group({
-      oldPassword: ["", [Validators.required, Validators.minLength(5)]],
-      newPassword: ["", [Validators.required, Validators.minLength(5)]],
-      newRePassword: ["", [Validators.required, Validators.minLength(5)]]
-    }, { validators: this.checkpassword })
-  }
-
-  resetPassword() {
-    if (this.resetPasswordForm.valid) {
-      this.isOk = false;
-      let resetPasswordModel: UserResetPasswordModel = this.resetPasswordForm.value;
-      resetPasswordModel.email = this.currentUser.email;
-      this.userService.resetPassword(resetPasswordModel,
-        (responseErr) => {
-          this.isOk = true;
-          this.messageService.showMessage(responseErr.error.message, { iconType: SweetIconType.Error });
-        }, (response) => {
-          this.isOk = true;
-          this.messageService.showMessage(response.message, { iconType: SweetIconType.Success });
-        })
-    }
-  }
-
-  checkpassword: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
-    let password = group.get("newPassword").value;
-    let repassword = group.get("newRePassword").value;
-    return password === repassword ? null : { notSame: true };
-  }
-  updateProfilePage() {
 
   }
-  resetPasswordPage() {
 
+  async updateProfilePage() {
+    const modal = await this.modalController.create({
+      component: UpdateProfilePage
+    })
+    return await modal.present();
+  }
+  async resetPasswordPage() {
+    const modal = await this.modalController.create({
+      component: ResetPasswordPage
+    })
+    return await modal.present();
   }
 }
