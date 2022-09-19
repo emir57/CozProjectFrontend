cefd704fe85dd5d4f5e8624236954d488528ed5e
emir57 <emir.gurbuz06@hotmail.com>
2022-09-19 16:27:54 +0300

    Configure QuestionModel

--- src/app/models/tables/questionModel.ts
+++ src/app/models/tables/questionModel.ts
@@ -1,14 +1,17 @@
 import { AnswerModel } from "./answerModel";
+import { CategoryModel } from "./categoryModel";
+import { User } from "./user";
 
 export interface QuestionModel {
   id?: number;
   content: string;
   teacherId: number;
+  user: User;
   categoryId: number;
+  category: CategoryModel;
   score: number;
   createdDate?: string;
   updatedDate?: string;
-  deletedDate?: string;
   answers?: AnswerModel[]
   result?: boolean;
 }
